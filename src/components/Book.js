a86a5147cf26aadb413670c360d31ab29f37103a
aubinleyoung <aubinsimpeze01@gmail.com>
2023-02-27 17:00:43 +0200

    Fix linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 const Book = ({ item }) => (
   <main className="pl-10 ">
-
     <div className="flex justify-between bg-white w-[90%] rounded-xl m-auto p-5">
       <div className="font-bold text-blue-500">
         <p>
@@ -14,9 +13,10 @@ const Book = ({ item }) => (
           Author:
           <span>{item.author}</span>
         </p>
-
       </div>
-      <button type="submit" className="add-btn bg-blue-500 rounded-xl p-5">Delete</button>
+      <button type="submit" className="add-btn bg-blue-500 rounded-xl p-5">
+        Delete
+      </button>
     </div>
   </main>
 );
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Categories = () => (
   <main className="w-full  flex flex-col items-center justify-center ">
-    <button className="bg-blue-600 mx-auto mt-10 text-white font-bold p-2 rounded-md" type="button">Check Status</button>
+    <button
+      className="bg-blue-600 mx-auto mt-10 text-white font-bold p-2 rounded-md"
+      type="button"
+    >
+      Check Status
+    </button>
   </main>
 );
 
